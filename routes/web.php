5b04a4eb9e705047ed5898f699838ea2c475c257
uldin-nl <info@uldin.nl>
2025-10-08 22:09:06 +0200

    fixes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+Route::get('/reverb', function () {
+    return response()->json(['status' => 'ok']);
+});
+
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
 require __DIR__ . '/tasting.php';
